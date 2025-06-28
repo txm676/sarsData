afc36a233c27a105dbf6bee91389fcec799dd373
Tom Matthews <txm676@gmail.com>
2025-06-28 14:20:58 +0000

    adding sar_build data

---
+++ R/data_aegean.R
@@ -0,0 +1,13 @@
+#' A SAR dataset describing *****
+#'
+#' A sample dataset in ***
+#'
+#' @usage data(IberianBirds)
+#' @format A list with two elements: data frame with 2 columns
+#'   and 90 rows. Each row contains the area of an island in the
+#'   Aegean (1st column) and the number of inverts on that island
+#'   (2nd column).
+#'@source ***
+#' @examples
+#' data(IberianBirds)
+"IberianBirds"
